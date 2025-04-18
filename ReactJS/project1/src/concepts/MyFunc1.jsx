035607854e57b1faca36dc090f727cecc7278eba
Abhijayshah <abhijayshah74@gmail.com>
2025-04-18 20:04:46 +0530

    third commit

---
+++ ReactJS/project1/src/concepts/MyFunc1.jsx
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>ABHIJAY KUMAR SHAH</title>
+</head>
+<body>
+    
+</body>
+</html>
